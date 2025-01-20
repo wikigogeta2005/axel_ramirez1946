54da5d85dc7bb599d36f840cc1f078840e49cd9d
Kenai Ramirez <kenawiki@gmail.com>
2025-01-19 22:43:32 -0500

    v2

--- Lenguaje c++/Arrays/Unidimensionales/ejercicio1.cpp
+++ Lenguaje c++/Arrays/Unidimensionales/ejercicio1.cpp
@@ -1,80 +1,72 @@
 #include <iostream>
-#include <stdlib.h>  //bibllioteca que genera numeros randomicos
-#include <time.h> //libreria para manejar el tiempo
+#include <cstdlib>  // Biblioteca para generar números aleatorios
+#include <ctime>    // Biblioteca para manejar el tiempo
 using namespace std;
 
-void llenardatos(int v[], int ne)
-{
-    srand(time(NULL)); //PARA GENERAR NUMEROS
-    for(int i=0; i<ne;i++)
-    {
-        v[i]=randfloat()%50+50;
+void llenardatos(float v[], int ne) {
+    srand(time(NULL)); // Inicializar el generador de números aleatorios
+    for (int i = 0; i < ne; i++) {
+        // Generar números aleatorios en el rango [50.0, 99.9] sin usar RAND_MAX
+        v[i] = 50.0 + rand() % 50 + (rand() % 10) / 10.0;
     }
 }
 
-void verDatos(int v[],int ne)
-{
-    cout<<"Los elemntos del vector son: "<<endl;
-    for(int i=0;i<ne;i++)
-    {
-        cout<<v[i]<<" ";
+void verDatos(float v[], int ne) {
+    cout << "Los elementos del vector son: " << endl;
+    for (int i = 0; i < ne; i++) {
+        cout << v[i] << " ";
     }
-
-
+    cout << endl;
 }
 
-float sumaPromediocifras(int v[],int ne)
-{
-    int i,suma=0;
-    for(i=0;i<ne;i++)
-    {
-        suma+=v[i];
+float sumaPromediocifras(float v[], int ne) {
+    float suma = 0;
+    for (int i = 0; i < ne; i++) {
+        suma += v[i];
     }
-    return (float)suma/ne;
+    return suma / ne;
 }
 
-int contardebajopromedio(int v[], int ne , float promedio)
-{
-    int contador=0;
-    for(int i=0;i<ne;i++)
-    {
-         if(v[i]<promedio)
-        {
-        contador++;
+int contardebajopromedio(float v[], int ne, float promedio) {
+    int contador = 0;
+    for (int i = 0; i < ne; i++) {
+        if (v[i] < promedio) {
+            contador++;
         }
-
     }
-   
     return contador;
-
 }
-void guardaelementosmenores(int v[],int ne, float promedioNumeros,int lista[])
-{
 
-    int i, j=0;
-    if(v[i]<promedioNumeros)
-    {
-        lista[j]=v[i];
-        j++;
+void guardaelementosmenores(float v[], int ne, float promedioNumeros, float lista[]) {
+    int j = 0;
+    for (int i = 0; i < ne; i++) {
+        if (v[i] < promedioNumeros) {
+            lista[j] = v[i];
+            j++;
+        }
     }
 }
 
-
-main()
-{
-    int ne,menores;
+int main() {
+    int ne, menores;
     float promedioNumeros;
-    cout<<"Cuantos elementos tiene el vector: ";
-    cin>>ne;
-    int vector[ne];
-    llenardatos(vector,ne);
-    verDatos(vector,ne);
-    promedioNumeros=sumaPromediocifras(vector,ne);
-    cout<<"El promedio de los datos generados es "<<promedioNumeros<<endl;
-    menores= contardebajopromedio(vector,ne,promedioNumeros);
-    cout<<"Los elementos debajo del promedio son: "<<menores;
-    int listamenores[menores];
-    guardaelementosmenores(vector,ne,promedioNumeros,listamenores);
-    verDatos(listamenores,menores);
 
+    cout << "Cuantos elementos tiene el vector: ";
+    cin >> ne;
+
+    float vector[ne];
+    llenardatos(vector, ne);
+    verDatos(vector, ne);
+
+    promedioNumeros = sumaPromediocifras(vector, ne);
+    cout << "El promedio de los datos generados es: " << promedioNumeros << endl;
+
+    menores = contardebajopromedio(vector, ne, promedioNumeros);
+    cout << "Los elementos debajo del promedio son: " << menores << endl;
+
+    float listamenores[menores];
+    guardaelementosmenores(vector, ne, promedioNumeros, listamenores);
+    verDatos(listamenores, menores);
+
+    return 0;
 }
